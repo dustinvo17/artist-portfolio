53ef162a23b188ecafa3007335f28c85a170a283
Dustin Vo <datvo.vtd@gmail.com>
2020-03-08 23:40:30 -0500

    fix message

--- js/main.js
+++ js/main.js
@@ -1015,7 +1015,7 @@ $window.on('resize', function(){
 	
 });
 $('.contact-form').submit(function(e) {
-		alert("Thank you for your information, I will get back to really soon!")
+		alert("Thank you for your information, I will get back to you in 2 business days!")
 })
 //end of IIFE function
 
